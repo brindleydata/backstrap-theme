10233af952425b52fe1239b9b9d355f8a67d205f
Daniel Luchinets <i@blackrabbit.info>
2021-04-30 04:18:52 +0300

    cleanup app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,6 @@ $(function () {
     // Selectpickers
     $('[data-toggle="selectpicker"]').selectpicker();
 
-    Livewire.on('update', function() {
-        $('[data-toggle="selectpicker"]').selectpicker();
-    });
-
-
     // Daterangepickers
     $('[data-toggle="daterangepicker"]').each(function(item) {
         if ($(this).data('mode') == 'single') {
@@ -49,6 +44,7 @@ $(function () {
         }
     });
 
+    // Hotpatch to make Livewire work
     $('[data-toggle="daterangepicker"]').on('hide.daterangepicker', function (event) {
         $(this)[0].dispatchEvent(new Event('input'))
     });
@@ -61,10 +57,10 @@ $(function () {
         html: true
     });
 
+    // Hide all tooltips when clicking on links (prevent stalled tooltips)
     $(document).on('click', 'a', function (event) {
         $('.tooltip').tooltip('hide');
     });
-
 
 
     // Legacy stuff
@@ -93,58 +89,4 @@ $(function () {
             });
         }
     });
-
-    $.getAllUrlParams = function (url) {
-        let obj = {};
-        let queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-
-        if (queryString) {
-            queryString = queryString.split('#')[0];
-
-            var arr = queryString.split('&');
-            for (var i = 0; i < arr.length; i++) {
-                var a = arr[i].split('=');
-                var paramName = a[0];
-                var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-                paramName = paramName.toLowerCase();
-                if (typeof paramValue === 'string') {
-                    paramValue = paramValue.toLowerCase();
-                }
-
-                // if the paramName ends with square brackets, e.g. colors[] or colors[2]
-                if (paramName.match(/\[(\d+)?\]$/)) {
-                    var key = paramName.replace(/\[(\d+)?\]/, '');
-                    if (!obj[key]) {
-                        obj[key] = [];
-                    }
-
-                    // if it's an indexed array e.g. colors[2]
-                    if (paramName.match(/\[\d+\]$/)) {
-                        // get the index value and add the entry at the appropriate position
-                        var index = /\[(\d+)\]/.exec(paramName)[1];
-                        obj[key][index] = paramValue;
-                    } else {
-                        // otherwise add the value to the end of the array
-                        obj[key].push(paramValue);
-                    }
-                } else {
-                    // we're dealing with a string
-                    if (!obj[paramName]) {
-                        // if it doesn't exist, create property
-                        obj[paramName] = paramValue;
-                    } else if (obj[paramName] && typeof obj[paramName] === 'string') {
-                        // if property does exist and it's a string, convert it to an array
-                        obj[paramName] = [obj[paramName]];
-                        obj[paramName].push(paramValue);
-                    } else {
-                        // otherwise add the property
-                        obj[paramName].push(paramValue);
-                    }
-                }
-            }
-        }
-
-        return obj;
-    }
 });
